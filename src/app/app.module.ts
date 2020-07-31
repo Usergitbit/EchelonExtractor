00bc3d35d15b9ed4e0057eaaf29aeb8d380b2125
Usergitbit <32998206+Usergitbit@users.noreply.github.com>
2020-08-01 00:17:40 +0300

    Integrated ng-open-cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgOpenCVModule, OpenCVOptions } from 'ng-open-cv';
+
+const openCVConfig: OpenCVOptions = {
+  scriptUrl: `assets/opencv/wasm/3.4/opencv.js`,
+  wasmBinaryFile: 'wasm/3.4/opencv_js.wasm',
+  usingWasm: true
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgOpenCVModule.forRoot(openCVConfig),
     BrowserAnimationsModule
   ],
   providers: [],
