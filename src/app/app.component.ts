cc7eacff9fd7735fafa1ded6821b2db6c69e27d9
Usergitbit <32998206+Usergitbit@users.noreply.github.com>
2020-07-31 22:04:44 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EchelonExtractor';
+
+  public image: any;
+
+
+  public onFileSelected(value: HTMLInputElement): void {
+
+    let file: File;
+    if (value == null || value.files == null || value.files.length == 0)
+      return;
+    else
+      file = value.files[0];
+
+    var fileReader: FileReader = new FileReader();
+
+    fileReader.onloadend = this.loadImage.bind(this);
+    fileReader.readAsDataURL(file);
+  }
+
+  private loadImage(data: ProgressEvent<FileReader>) :void {
+    if (data != null && data.target != null)
+      this.image = data.target.result;
+  }
+
 }
