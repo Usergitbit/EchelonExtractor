71e5769f2f8d652fdc64570e4b19351ecf14f469
Usergitbit <32998206+Usergitbit@users.noreply.github.com>
2020-11-22 12:25:23 +0200

    Update CLI

--- src/app/components/echelon-canvas/echelon-canvas.component.spec.ts
+++ src/app/components/echelon-canvas/echelon-canvas.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EchelonCanvasComponent } from './echelon-canvas.component';
 
@@ -6,7 +6,7 @@ describe('EchelonCanvasComponent', () => {
   let component: EchelonCanvasComponent;
   let fixture: ComponentFixture<EchelonCanvasComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EchelonCanvasComponent ]
     })
--- src/app/components/message-snack-bar/message-snack-bar.component.spec.ts
+++ src/app/components/message-snack-bar/message-snack-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MessageSnackBarComponent } from './message-snack-bar.component';
 
@@ -6,7 +6,7 @@ describe('MessageSnackBarComponent', () => {
   let component: MessageSnackBarComponent;
   let fixture: ComponentFixture<MessageSnackBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MessageSnackBarComponent ]
     })
